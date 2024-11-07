6c6e70c674c68dffe08250f8a70fc9363b241164
PatoGarcia91 <pfernando.garcia@gmail.com>
2024-11-07 19:34:14 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
 import Navbar from './components/navbar/Navbar.jsx'
 
